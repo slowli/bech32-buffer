8c845b2a2b97cd9dfb21b84ec649bd9bacb7d1bb
Alex Ostrovski <ostrovski.alex@gmail.com>
2017-07-30 15:55:36 +0300

    Remove `forEach` for typed arrays
    (Not present in PhantomJS.)

--- src/encoding.js
+++ src/encoding.js
@@ -44,13 +44,13 @@ const GEN = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3];
 function polymod(values: FiveBitArray): number {
   let chk = 1;
 
-  values.forEach((v) => {
+  for (let vi = 0; vi < values.length; vi++) {
     const b = (chk >> 25);
-    chk = ((chk & 0x1ffffff) << 5) ^ v;
+    chk = ((chk & 0x1ffffff) << 5) ^ values[vi];
     for (let i = 0; i < GEN.length; i++) {
       if (((b >> i) & 1) === 1) chk ^= GEN[i];
     }
-  });
+  }
 
   return chk;
 }
